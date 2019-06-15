6986f96d47acfb1f488f46b6dbf9562d0081714e
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-15 22:50:35 +0545

    make all question model fields blank=False except for hint make the *count fields non-editable and set the default value to 0

--- questions/models.py
+++ questions/models.py
@@ -5,15 +5,19 @@ from django.db import models
 class Question(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated= models.DateTimeField(auto_now=True)
-    num = models.CharField(unique=True,max_length=255)
-    question = models.TextField(max_length=500)
-    opt_a = models.CharField(max_length=255)
-    opt_b = models.CharField(max_length=255)
-    opt_c = models.CharField(max_length=255)
-    opt_d = models.CharField(max_length=255)
-    hint = models.TextField(max_length=500)
-    answer = models.CharField(max_length=1)
-    right_count = models.PositiveIntegerField()
-    wrong_count = models.PositiveIntegerField()
-    total_right_count = models.PositiveIntegerField()
-    total_wrong_count = models.PositiveIntegerField()
+    num = models.CharField(unique=True,max_length=255,blank=False)
+    question = models.TextField(max_length=500,blank=False)
+    opt_a = models.CharField(max_length=255,blank=False)
+    opt_b = models.CharField(max_length=255,blank=False)
+    opt_c = models.CharField(max_length=255,blank=False)
+    opt_d = models.CharField(max_length=255,blank=False)
+    hint = models.TextField(max_length=500,blank=True)
+    answer = models.CharField(max_length=1,blank=False)
+    right_count = models.PositiveIntegerField(
+            default=0,blank=False,editable=False)
+    wrong_count = models.PositiveIntegerField(
+            default=0,blank=False,editable=False)
+    total_right_count = models.PositiveIntegerField(
+            default=0,blank=False,editable=False)
+    total_wrong_count = models.PositiveIntegerField(
+            default=0,blank=False,editable=False)
