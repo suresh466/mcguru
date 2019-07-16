000480646d81606153c532a0f17dd7361ae64c5d
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-07-17 00:45:19 +0545

    remove question sorting function get questions on the basis of question number adapt answer view to work with different categories properly

--- questions/views.py
+++ questions/views.py
@@ -1,14 +1,10 @@
 from django.contrib import messages
 from django.utils import timezone
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist,MultipleObjectsReturned
 from django.shortcuts import render,redirect,get_object_or_404
 from .forms import QuestionAddForm
 from .models import Info,Question
 # Create your views here.
-
-MAX_WRONG_COUNT = 15
-MAX_RIGHT_COUNT = 2
-MAX_ITERATIONS = 15
 
 def question_add(request):
     template='questions/add.html'
@@ -62,76 +58,65 @@ def question_add(request):
 
     return render(request,template,context)
 
-def sort():
-    for wrong_count in reversed(range(0,MAX_WRONG_COUNT+1)):
-        queryset = Question.objects.filter(wrong_count=wrong_count)
-        for query in queryset:
-            query.date_created=timezone.now()
-            query.save()
-
-def get_first_question():
-    queryset = Question.objects.all()
-    min_time = queryset.first().date_created
-    for query in queryset:
-        if query.date_created<min_time:
-            min_time = query.date_created
-    return Question.objects.get(date_created=min_time)
-
-def get_correct_answer(answer_num,question):
-    if answer_num == 'a':
+def get_correct_answer(answered_num,question):
+    if answered_num == 'a' or '1':
         answer = question.opt_a
-    elif answer_num == 'b':
+    elif answered_num == 'b' or '2':
         answer = questin.opt_b
-    elif answer_num == 'c':
+    elif answered_num == 'c' or '3':
         answer = question.opt_c
-    else:
+    elif answered_num == 'd' or '4':
         answer = question.opt_d
     return answer
 
 def answer(request):
     template='questions/answer.html'
-    
+
     info = get_object_or_404(Info, identifier=1)
+    category = request.path_info.replace('/','')
+    if category == '':
+        category = 'uncategorized'
+
+    category_last_answered= getattr(info, 'last_answered_'+ category)
     
-    if info.last_answered == 0:
-        question = get_first_question()
+    if category_last_answered == 0:
+        question = Question.objects.get(category=category.capitalize(),question_num=1)
     else:
-        last_answered = Question.objects.get(pk=info.last_answered)
-        if info.iteration_num == MAX_ITERATIONS:
-            messages.warning(request,
-                    "You have done max iterations of this question set, please update max_iterations if you want to keep going.")
-            return redirect('about')
         try:
-            question = last_answered.get_next_by_date_created()
+            question = Question.objects.get(question_num=category_last_answered+1, category=category.capitalize())
+
+        except MultipleObjectsReturned:
+            print('===============================++++++++++++++++=========================')
+            print('There is more than one question with question number {} in the {} category fix it.'
+                    .format(category_last_answered+1,category))
+            print('===============================++++++++++++++++=========================')
+            raise
+
         except ObjectDoesNotExist:
-            deleted = Question.objects.filter(right_count=MAX_RIGHT_COUNT).delete()
-            sort()
-            info.total_questions -= deleted[0]
-            info.last_answered = 0
-            info.iteration_num += 1
+            messages.success(request,
+                    "Congratulations all questions are done in this category.")
+            setattr(info, 'last_answered_'+category, 0)
             info.save()
-            return redirect('home')
+            return redirect('about')
 
     if request.method == 'POST':
-        answered_num = request.POST['answer']
-        answer_num = question.answer
-        answer = get_correct_answer(answer_num,question)
+        answered_num = request.POST['answer'].lower()
+        answer = get_correct_answer(answered_num,question)
 
-        if answered_num == question.answer:
+        if answer == question.answer:
             question.right_count += 1
             question.total_right_count += 1
-            question.save()
             messages.success(request,
                     "Congratulations, correct answer is {}: {}."
-                    .format(answer_num,answer))
+                    .format(answered_num,answer))
         else:
             question.wrong_count += 1
             question.total_wrong_count += 1
-            question.save()
             messages.warning(request,
-                    "The correct answer was {}: {}. but you selected {}. Good luck for this one."
-                    .format(answer_num,answer,answered_num))
-        info.last_answered = question.pk
+                    "The correct answer was : {}. but you selected {}. Good luck for this one."
+                    .format(answer,answered_num))
+        question.save()
+        setattr(info, 'last_answered_'+category, question.question_num)
         info.save()
         return redirect('home')
 
